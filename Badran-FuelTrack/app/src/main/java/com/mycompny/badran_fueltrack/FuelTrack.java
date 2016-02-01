6a82bb4453c1c22e758da0c0f399a3ed17f3120f
badran047 <hasan_badran@windowslive.com>
2016-02-01 02:44:33 -0700

    Edit before last edit for submission

--- Badran-FuelTrack/app/src/main/java/com/mycompny/badran_fueltrack/FuelTrack.java
+++
@@ -1,7 +0,0 @@
-package com.mycompny.badran_fueltrack;
-
-/**
- * Created by hasan on 2016-01-29.
- */
-public class FuelTrack {
-}
---
+++ as1/README.txt
@@ -0,0 +1,8 @@
+Android App: Badran-FuelTrack
+Course: CMPUT 301
+
+
+
+YouTube Video URL:
+https://youtu.be/-PTajo03EWM
+
